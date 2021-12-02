4dbd2ff8ad2930fe191a0e74bdfa6cbcba0f6bbf
Rafael Zelli <rafazelli@gmail.com>
2021-12-02 17:00:08 -0300

    FEAT: Persistindo os dados

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,9 +1,12 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimento', (req, res) => 
         res.send('Você esta na rota de atendimentos e está realizando um GET'))
 
     app.post('/atendimento', (req,res) => {
-        console.log(req.body)
+        const atendimento = req.body
+        Atendimento.adiciona(atendimento)
         res.send("Voçê esta na rota de atendimento e está realizando um POST")
     })
 }
